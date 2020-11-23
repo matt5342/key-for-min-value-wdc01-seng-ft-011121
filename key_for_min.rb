84f4c7093fca65c17e25e7f5f1fd306b8d9cd89a
Matthew Sewell <mattfsewell@gmail.com>
2020-11-23 22:53:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   key_for_min_value = nil
   num = 10000
   name_hash.each do |key, value|
-    if name_hash != nil && name_hash[key][value] < num
+    if name_hash[key][value] < num
       num = name_hash[key][value]
       key_for_min_value = name_hash[key]
  
